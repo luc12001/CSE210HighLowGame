4aaa5849fe703b971b2b61050cc8b38412684de3
luc12001 <luc12001@byui.edu>
2022-02-12 11:15:50 -0600

    Added methods to director class for guessing and lower and higher

---
+++ hilo/game/director.py
@@ -0,0 +1,81 @@
+
+
+
+
+
+
+
+        if guess.lower() == "h" and card2 > card1:
+            print("Correct!, you won 100 more points.")
+            self.points += 100
+            print("Your score so far is", self.points)
+            print()
+            if self.points <= 0:
+                print("Game Over!")
+                print("Thank you for playing")
+                quit()
+            if self.again() == "y":
+                pass
+            else:
+                quit()
+
+        if guess.lower() == "h" and card2 < card1:
+            print("Wrong guess!, you lost 75 points")
+            self.points -= 75
+            print("Your score so far is", self.points)
+            print()
+            if self.points <= 0:
+                print("Game Over!")
+                print("Thank you for playing")
+                quit()
+            if self.again() == "y":
+                pass
+            else:
+                quit()
+
+        if guess.lower() == "l" and card2 < card1:
+            print("Correct!, you won 100 more points")
+            self.points += 100
+            print("Your score so far is", self.points)
+            print()
+            if self.points <= 0:
+                print("Game Over!")
+                print("Thank you for playing")
+                quit()
+            if self.again() == "y":
+                pass
+            else:
+                quit()
+
+        if guess.lower() == "l" and card2 > card1:
+            print("Wrong guess!, you lost 75 points")
+            self.points -= 75
+            print("Your score so far is", self.points)
+            print()
+            if self.points <= 0:
+                print("Game Over!")
+                print("Thank you for playing.")
+                quit()
+            if self.again() == "y":
+                pass
+            else:
+                quit()
+
+        if self.points <= 0:
+            print("You ran out of points! Game Over!")
+            print("Thank you for playing")
+            quit()
+
+
+
+def main():
+    '''
+    Calls the card and director function. Then draws the cards and makes sure the game is started. 
+    '''
+    card = Card()
+    person = Director()
+    card.draw()
+    person.start_game()
+
+if __name__ == "__main__":
+    main()
